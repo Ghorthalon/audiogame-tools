8ece3a8cbfb1b535985099d97ba6a7ec413a89e4
Christopher Toth <q.alpha@gmail.com>
2022-07-16 16:58:30 -0600

    Made Scene an abstract class with some documentation rather than an interface

--- engine/scene/scene.ts
+++ engine/scene/scene.ts
@@ -1,12 +1,29 @@
-import { Game } from '../game';
 import { SceneManager } from './manager';
 
-export interface Scene {
+
+
+export abstract class Scene {
 	id: string;
 	data: any;
-	onActivate(manager: SceneManager);
-	onDeactivate();
-	onSwitch(data: any);
-	update(dt: number);
-	updateDraw();
+
+	// Called when the scene is activated
+	onActivate(manager: SceneManager): void {
+		console.log(`Scene ${this.id} activated`);
+	}
+
+	// Called when the scene is deactivated
+	onDeactivate(): void {
+		console.log(`Scene ${this.id} deactivated`);
+	}
+
+	// Called when the scene is switched to but not yet activated.
+	onSwitch(data: any): void {
+		this.data = data;
+	}
+
+	// Called every frame
+	abstract update(dt: number): void;
+
+	// Called when the update.draw event is fired
+	abstract updateDraw(): void;
 }

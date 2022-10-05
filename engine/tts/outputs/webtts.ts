1d5859c04e99e88a4ea20a7319b66f314295ca09
Quin <quin.marilyn05@gmail.com>
2022-10-05 16:41:24 -0600

    Improve TTS module.
    Finish WebTTS output.
    
    Make the factory return instances of outputs.

--- engine/tts/outputs/webtts.ts
+++ engine/tts/outputs/webtts.ts
@@ -1,3 +1,26 @@
 import { BaseOutput } from './base-output';
 
-export class WebTTSOutput extends BaseOutput {}
+export class WebTTSOutput extends BaseOutput {
+	private synth: Window['speechSynthesis'];
+	private rate: number;
+
+	public constructor(options: any = {}) {
+		super();
+		this.rate = options.rate || 1;
+		this.synth = window.speechSynthesis;
+	}
+
+	public speak(text: string): void {
+		let utterThis = new SpeechSynthesisUtterance(text);
+		utterThis.rate = this.rate;
+		this.synth.speak(utterThis);
+	}
+
+	public stop(): void {
+		this.synth.cancel();
+	}
+
+	public setOptions(options: any): void {
+		this.rate = options.rate || 1;
+	}
+}

bf91f9544ed834d46b99a40e1ff03babddac96d5
Talon <talon@iamtalon.me>
2024-04-06 14:29:17 +0200

    Lower the gain of reverbed convolution by default

--- engine/resonator/effects/convolver.ts
+++ engine/resonator/effects/convolver.ts
@@ -6,6 +6,7 @@ export default class Convolver extends BaseEffect {
 	private buffer: AudioBuffer;
 	private channelSplitter: ChannelSplitterNode;
 	private channelMerger: ChannelMergerNode;
+	private outputGain: GainNode;
 	constructor(context: ResonatorAudioContext, graph: AudioGraph, params: any) {
 		super(context, graph, params);
 		console.log(`Creating convolver`);
@@ -21,8 +22,11 @@ export default class Convolver extends BaseEffect {
 		this.channelSplitter.connect(this.channelMerger, 1, 0);
 		this.channelSplitter.connect(this.channelMerger, 0, 1);
 		this.channelSplitter.connect(this.channelMerger, 1, 1);
+		this.outputGain = this.context.getContext().createGain();
+		this.outputGain.gain.setValueAtTime(0.5, this.context.getContext().currentTime);
 		node.connect(this.channelSplitter);
-		this.channelMerger.connect(this.effectNode);
+		this.channelMerger.connect(this.outputGain);
+		this.outputGain.connect(this.effectNode);
 		this.inputNode = node;
 	}
 }
